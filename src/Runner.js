353a8e8ceb6717881e3cc5319c573d4e60f9def2
Shir Brass <shirbr510@users.noreply.github.com>
2017-10-30 12:20:31 +0200

    Hotfix/es broken build (#3)
    * removed es6 from eslint env
    
    * let's try removing all the es shit
    
    * Revert "let's try removing all the es shit"
    
    This reverts commit c7766c5b94af5bfa90d23e5b77bdae7c1a247515.
    
    * Revert "removed es6 from eslint env"
    
    This reverts commit 99270d4340bd6249d5407bd9042afc06df5ee5b2.
    
    * reverted package json version
    
    * uninstall es airbnb
    
    * added missing babel-eslint
    
    * I've had enough of this. final try by removing eslint from scripts
    
    * fix runner and package json to actually fail
    
    * Removed crappy package-lock.json & added it to ignore

--- src/Runner.js
+++ src/Runner.js
@@ -1,13 +1,13 @@
 // @flow
 
-import type { ValidationResult, ValidationRule, ValidationRules } from "./Types"
+import type {ValidationResult, ValidationRule, ValidationRules} from "./Types"
 
-export const DEFAULT_INVALID_FIELD_MESSAGE='field is invalid';
+export const DEFAULT_INVALID_FIELD_MESSAGE: string = 'field is invalid';
 
 export const runRule = (validationRule: ValidationRule, value: any): string => {
     if (validationRule) {
         const isValid = validationRule.func(value);
-        if(!isValid){
+        if (!isValid) {
             return validationRule.message || DEFAULT_INVALID_FIELD_MESSAGE;
         }
     }
@@ -15,19 +15,19 @@ export const runRule = (validationRule: ValidationRule, value: any): string => {
     return null;
 };
 
-export const runValidations = (validationRules: ValidationRules = {}, objectToValidate: object): ValidationResult =>{
-  if(!validationRules){
-    return {};
-  }
-  return Object.keys(validationRules).reduce((validationObject: ValidationResult, fieldName: string | number) => {
-      const validationError = runRule(validationRules[fieldName], objectToValidate[fieldName]);
-      if (!validationError) {
-        delete validationObject[fieldName];
-      }
-      else {
-        validationObject[fieldName] = validationError;
-      }
-      return validationObject;
+export const runValidations = (validationRules: ValidationRules = {}, objectToValidate: object): ValidationResult => {
+    if (!validationRules) {
+        return {};
     }
-    , {})
+    return Object.keys(validationRules).reduce((validationObject: ValidationResult, fieldName: string | number) => {
+            const validationError = runRule(validationRules[fieldName], objectToValidate[fieldName]);
+            if (!validationError) {
+                delete validationObject[fieldName];
+            }
+            else {
+                validationObject[fieldName] = validationError;
+            }
+            return validationObject;
+        }
+        , {})
 };
